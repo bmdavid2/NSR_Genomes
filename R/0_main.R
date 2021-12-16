bdcd4c0bd10a8f68b1a4f91376dd674ccf1a0f06
Ben David <66976120+bmdavid2@users.noreply.github.com>
2021-12-16 13:41:30 -0600

    Added proper dependencies

---
+++ R/0_main.R
@@ -0,0 +1,13 @@
+#' @import   genbankr
+#' @import kmer
+#' @import Biostrings
+#' @import insect
+#' @import tictoc
+#' @import plyr
+#' @import dplyr
+#' @import purrr
+#' @import stringr
+#' @import snow
+#' @import rentrez
+#' @import primer3
+NULL
